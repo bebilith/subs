8fc97651fd6390ae7a3edd3f52ba02d5c7460910
till <tillothy@gmail.com>
2014-10-08 09:37:41 +0200

    inital commit

---
+++ desktop/src/com/warrensofthought/subs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.warrensofthought.subs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.warrensofthought.subs.Subs;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Subs(), config);
+    }
+}
